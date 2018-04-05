cb034e7ca9a905be3831bc78449cd6e495967f75
Teemu Peltola <teepelto@gmail.com>
2018-04-05 04:39:59 +0000

    add total count to expense summary

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -5,15 +5,15 @@ import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral';
 
-export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
-    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+export const ExpensesSummary = ({ expenseCount, expenseCountAll, expensesTotal }) => {
+    const expenseWord = expenseCountAll === 1 ? 'expense' : 'expenses';
     const formattedTotal = numeral(expensesTotal).format('$0,0.00');
     
     return (
         <div className="page-header">
             <div className="content-container">
                 <h1 className="page-header__title">
-                    Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedTotal}</span>.
+                    Viewing <span>{expenseCount}</span> out of <span>{expenseCountAll}</span> {expenseWord} totalling <span>{formattedTotal}</span>.
                     <div className="page-header__actions">
                         <Link className="button" to="/create">Add Expense</Link>
                     </div>
@@ -27,6 +27,7 @@ const mapStateToProps = (state) => {
     const expenses = selectExpenses(state.expenses, state.filters);
     return {
         expenseCount: expenses.length,
+        expenseCountAll: state.expenses.length,
         expensesTotal: selectExpensesTotal(expenses) / 100
     };
 };
